314ec598eaeb07305f2481b5d90e668a62beee46
mrcicil <rahmatkhairi91@gmail.com>
2025-05-24 15:37:28 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit More <code>src/App.jsx</code> and save to test HMR
+          Edit More editing this for pull<code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
