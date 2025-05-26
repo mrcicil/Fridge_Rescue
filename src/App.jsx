60efbb18900f779ad102953afcbffb02df293e3c
mrcicil <rahmatkhairi91@gmail.com>
2025-05-26 19:14:51 +0800

    adding tailwind and template

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Homepage from './routes/Homepage'
+import Login from './routes/Login'
+import Company from './routes/Company'
+import Header from './routes/Header'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-     
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="text-3xl font-bold underline">
-          I want to push this to main branch<code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Header/>}>
+
+      <Route path='Homepage' element={<Homepage/>}/>
+      <Route path='login' element={<Login></Login>}/>
+      <Route path='company' element={<Company></Company>}/>
+
+    
+      </Route>
+    </Routes>
+
+
+    </BrowserRouter>
+    
   )
 }
 
