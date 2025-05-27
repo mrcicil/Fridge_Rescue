8e22d46cf6ca718bbaacaf12116dc971aa95397f
mrcicil <rahmatkhairi91@gmail.com>
2025-05-27 12:28:32 +0800

    update login page

---
+++ src/context/Authcontext.jsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+    navigate("/");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
