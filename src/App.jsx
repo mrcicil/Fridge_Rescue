d9c29fa2e7e93bd25ea2f6f797e4f1bc4f7cf3fb
Mashwire <indy@mashwire.com.sg>
2025-05-29 15:08:32 +0800

    merge conflict app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import IngredientInput from './components/IngredientInput';
-import './App.css';
-import fridgeLogo from './assets/fridge_rescue.png';
-
-function App() {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  const handleSearch = async (ingredients) => {
-    try {
-      setLoading(true);
-      setError(null);
-      /*fetch api and display results*/
-    } catch (err) {
-      setError('Failed to fetch recipes. Please try again.');
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  return (
-    <div className="app">
-      <div className="fridgeLogoimg">
-        <img src={fridgeLogo} alt="Fridge Rescue" />
-      </div>
-      <p className="app-description">
-        Reduce food waste by finding recipes with ingredients you already have!
-      </p>
-      
-      <IngredientInput onSearch={handleSearch} />
-      
-      {loading && <div className="loading">Loading...</div>}
-      {error && <div className="error">{error}</div>}
-    </div>
-  );
-=======
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -76,7 +38,6 @@ function App() {
    
     
   )
->>>>>>> main
 }
 
-export default App;
+export default App
